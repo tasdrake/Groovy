2ee171956884a8bbb6b1b4e50cc52da456560b2d
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-10-15 16:21:22 -0600

    test 1 passing

--- gigasecond/Gigasecond.groovy
+++ gigasecond/Gigasecond.groovy
@@ -1,10 +1,9 @@
 class Gigasecond {
 
-    static def from(start) {
-
-      Date date = new Date(millis.getTime() + 1000000000000);
-      SimpleDateFormat sdf = new SimpleDateFormat("EEEE,MMMM d,yyyy h:mm a");
-      return sdf.format(date);
+  Date from(Date date) {
+    use (groovy.time.TimeCategory) {
+      return date + 1000000000.seconds;
     }
+  }
 
 }
