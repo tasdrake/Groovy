74ce092f5a86ef52897318847ebdc3b4a49c090f
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-10-15 16:16:07 -0600

    exer2 done

---
+++ hamming/Hamming.groovy
@@ -0,0 +1,26 @@
+class Hamming {
+
+    static def main(String[] args) {
+      def hamming = new Hamming();
+      println( new Hamming().compute('AGG','AAAACTGACCCACCCCAGG'));
+    }
+
+    def compute(strand1, strand2) {
+        def count = 0;
+        def length = strand1.size() > strand2.size() ? strand2.size() : strand1.size();
+
+        def test = {s1, s2 -> s1 != s2 ? count ++ : null}
+
+        for (def i = 0; i < length; i++) {
+            // if (test(strand1[i], strand2[i])) {
+            //     count++;
+            // }
+            test(strand1[i], strand2[i]);
+        }
+
+
+
+        return count;
+    }
+
+}
